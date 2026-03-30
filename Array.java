bf3262ab878dafd95fc2342e5e42add83c264805
Aaliyah <aaliyah.gabona.strathmore.edu.com>
2026-03-30 10:11:05 +0300

    initial commit

---
+++ Array.java
@@ -0,0 +1,23 @@
+
+public class Array {
+    
+    import java.io.File;     
+   import java.io.IOException; 
+   public static void main(String[] args) {
+    ArrayList<String> users = new ArrayList<String>();
+    users.add("passenger");
+    users.add("driver");
+    users.add("parcelname");
+    users.add("feedback");
+    System.out.println(users);
+  }
+
+
+
+
+
+
+   
+    
+}
+
